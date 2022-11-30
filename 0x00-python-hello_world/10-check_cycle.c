f43aeee4362c0ac1e7062bc4fe637489627b0a2c
Davewonder Emmanson <davidemmanson001@gmail.com>
2022-11-30 15:46:02 -0800

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,7 +14,7 @@ int check_cycle(listint_t *list)
 
 	if (!list)
 		return (0);
-	while (former && later->next);
+	while (former && later &&  later->next);
 	{
 		former = former->next;
 		later = later->next;
