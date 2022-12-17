967a9de58d306233c5587c67f9094bfc19aa72d0
3mmanson <emmansonstronger@gmail.com>
2022-12-17 07:02:24 +0100

    Retrace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return
-    new_string = ""
-    for c in range(len(my_string)):
-        if my_string[c] != 'c' and my_string[c] != 'C':
-            new_string = new_string + my_string[c]
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
